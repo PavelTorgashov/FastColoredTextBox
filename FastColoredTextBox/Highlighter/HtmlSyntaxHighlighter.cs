56eefb7abc106ee97f5d59684dcbb564495a35dc
Mirko Flessenkaemper <mirkofl16@web.de>
2015-04-11 01:56:47 +0200

    HtmlSyntaxHighlighter implementing base class Syntaxhighlighter

---
+++ FastColoredTextBox/Highlighter/HtmlSyntaxHighlighter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastColoredTextBoxNS.Highlighter
+{
+    class HtmlSyntaxHighlighter : SyntaxHighlighter
+    {
+        public HtmlSyntaxHighlighter()
+        {
+        }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void HighlightSyntax(Range range)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AutoIndentNeeded(object sender, AutoIndentEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
