964f53a6823cca51edad39f0a44df08bdf08cedb
Mirko Flessenkaemper <mirkofl16@web.de>
2015-04-11 01:38:45 +0200

    Extracting all functions witch are called from outside into an interface

---
+++ FastColoredTextBox/ISyntaxHighlighter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FastColoredTextBoxNS
+{
+    interface ISyntaxHighlighter : IDisposable
+    {
+        void HighlightSyntax(Range range);
+        void AutoIndentNeeded(object sender, AutoIndentEventArgs args);
+    }
+}
