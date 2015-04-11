942f9fcc4e596b6487c156d5150e939a7616a25d
Mirko Flessenkaemper <mirkofl16@web.de>
2015-04-12 01:52:02 +0200

    Class for CustomSyntaxHighlighter from XML File

---
+++ FastColoredTextBox/Highlighter/CustomSyntaxHighlighter.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace FastColoredTextBoxNS.Highlighter
+{
+    class CustomSyntaxHighlighter : SyntaxHighlighter
+    {
+        public CustomSyntaxHighlighter(string XMLdescriptionFile)
+        {
+            if (!File.Exists(XMLdescriptionFile))
+            {
+                XMLdescriptionFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Path.GetFileName(XMLdescriptionFile));
+
+                if (!File.Exists(XMLdescriptionFile))
+                    throw new ArgumentException();
+            }
+
+            var doc = new XmlDocument();
+            doc.LoadXml(File.ReadAllText(XMLdescriptionFile));
+            this.SyntaxDesc = ParseXmlDescription(doc);
+        }
+
+        public override void HighlightSyntax(Range range)
+        {
+            CustomHighlightSyntax(range);
+        }
+
+        public override void AutoIndentNeeded(object sender, AutoIndentEventArgs args)
+        {
+            CustomAutoIndentNeeded(sender, args);
+        }
+
+        public override void Dispose()
+        {
+                this.SyntaxDesc.Dispose();
+        }
+
+        public override void setTextBoxParameter(FastColoredTextBox tb)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CustomAutoIndentNeeded(object sender, AutoIndentEventArgs args)
+        {
+            var tb = sender as FastColoredTextBox;
+            tb.CalcAutoIndentShiftByCodeFolding(sender, args);
+        }
+
+        private void CustomHighlightSyntax(Range range)
+        {
+            //set style order
+            range.tb.ClearStylesBuffer();
+            for (int i = 0; i < this.SyntaxDesc.styles.Count; i++)
+                range.tb.Styles[i] = this.SyntaxDesc.styles[i];
+            //brackets
+            char[] oldBrackets = RememberBrackets(range.tb);
+            range.tb.LeftBracket = this.SyntaxDesc.leftBracket;
+            range.tb.RightBracket = this.SyntaxDesc.rightBracket;
+            range.tb.LeftBracket2 = this.SyntaxDesc.leftBracket2;
+            range.tb.RightBracket2 = this.SyntaxDesc.rightBracket2;
+            //clear styles of range
+            range.ClearStyle(this.SyntaxDesc.styles.ToArray());
+            //highlight syntax
+            foreach (RuleDesc rule in this.SyntaxDesc.rules)
+                range.SetStyle(rule.style, rule.Regex);
+            //clear folding
+            range.ClearFoldingMarkers();
+            //folding markers
+            foreach (FoldingDesc folding in this.SyntaxDesc.foldings)
+                range.SetFoldingMarkers(folding.startMarkerRegex, folding.finishMarkerRegex, folding.options);
+
+            RestoreBrackets(range.tb, oldBrackets);
+        }
+
+        private static SyntaxDescriptor ParseXmlDescription(XmlDocument doc)
+        {
+            var desc = new SyntaxDescriptor();
+            XmlNode brackets = doc.SelectSingleNode("doc/brackets");
+            if (brackets != null)
+            {
+                if (brackets.Attributes["left"] == null || brackets.Attributes["right"] == null ||
+                    brackets.Attributes["left"].Value == "" || brackets.Attributes["right"].Value == "")
+                {
+                    desc.leftBracket = '\x0';
+                    desc.rightBracket = '\x0';
+                }
+                else
+                {
+                    desc.leftBracket = brackets.Attributes["left"].Value[0];
+                    desc.rightBracket = brackets.Attributes["right"].Value[0];
+                }
+
+                if (brackets.Attributes["left2"] == null || brackets.Attributes["right2"] == null ||
+                    brackets.Attributes["left2"].Value == "" || brackets.Attributes["right2"].Value == "")
+                {
+                    desc.leftBracket2 = '\x0';
+                    desc.rightBracket2 = '\x0';
+                }
+                else
+                {
+                    desc.leftBracket2 = brackets.Attributes["left2"].Value[0];
+                    desc.rightBracket2 = brackets.Attributes["right2"].Value[0];
+                }
+
+                if (brackets.Attributes["strategy"] == null || brackets.Attributes["strategy"].Value == "")
+                    desc.bracketsHighlightStrategy = BracketsHighlightStrategy.Strategy2;
+                else
+                    desc.bracketsHighlightStrategy = (BracketsHighlightStrategy)Enum.Parse(typeof(BracketsHighlightStrategy), brackets.Attributes["strategy"].Value);
+            }
+
+            var styleByName = new Dictionary<string, Style>();
+
+            foreach (XmlNode style in doc.SelectNodes("doc/style"))
+            {
+                Style s = ParseStyle(style);
+                styleByName[style.Attributes["name"].Value] = s;
+                desc.styles.Add(s);
+            }
+            foreach (XmlNode rule in doc.SelectNodes("doc/rule"))
+                desc.rules.Add(ParseRule(rule, styleByName));
+            foreach (XmlNode folding in doc.SelectNodes("doc/folding"))
+                desc.foldings.Add(ParseFolding(folding));
+
+            return desc;
+        }
+
+        private static Style ParseStyle(XmlNode styleNode)
+        {
+            XmlAttribute typeA = styleNode.Attributes["type"];
+            XmlAttribute colorA = styleNode.Attributes["color"];
+            XmlAttribute backColorA = styleNode.Attributes["backColor"];
+            XmlAttribute fontStyleA = styleNode.Attributes["fontStyle"];
+            XmlAttribute nameA = styleNode.Attributes["name"];
+            //colors
+            SolidBrush foreBrush = null;
+            if (colorA != null)
+                foreBrush = new SolidBrush(ParseColor(colorA.Value));
+            SolidBrush backBrush = null;
+            if (backColorA != null)
+                backBrush = new SolidBrush(ParseColor(backColorA.Value));
+            //fontStyle
+            FontStyle fontStyle = FontStyle.Regular;
+            if (fontStyleA != null)
+                fontStyle = (FontStyle)Enum.Parse(typeof(FontStyle), fontStyleA.Value);
+
+            return new TextStyle(foreBrush, backBrush, fontStyle);
+        }
+
+        private static FoldingDesc ParseFolding(XmlNode foldingNode)
+        {
+            var folding = new FoldingDesc();
+            //regex
+            folding.startMarkerRegex = foldingNode.Attributes["start"].Value;
+            folding.finishMarkerRegex = foldingNode.Attributes["finish"].Value;
+            //options
+            XmlAttribute optionsA = foldingNode.Attributes["options"];
+            if (optionsA != null)
+                folding.options = (RegexOptions)Enum.Parse(typeof(RegexOptions), optionsA.Value);
+
+            return folding;
+        }
+
+        private static RuleDesc ParseRule(XmlNode ruleNode, Dictionary<string, Style> styles)
+        {
+            var rule = new RuleDesc();
+            rule.pattern = ruleNode.InnerText;
+            //
+            XmlAttribute styleA = ruleNode.Attributes["style"];
+            XmlAttribute optionsA = ruleNode.Attributes["options"];
+            //Style
+            if (styleA == null)
+                throw new Exception("Rule must contain style name.");
+            if (!styles.ContainsKey(styleA.Value))
+                throw new Exception("Style '" + styleA.Value + "' is not found.");
+            rule.style = styles[styleA.Value];
+            //options
+            if (optionsA != null)
+                rule.options = (RegexOptions)Enum.Parse(typeof(RegexOptions), optionsA.Value);
+
+            return rule;
+        }
+
+        private static Color ParseColor(string s)
+        {
+            if (s.StartsWith("#"))
+            {
+                if (s.Length <= 7)
+                    return Color.FromArgb(255,
+                                          Color.FromArgb(Int32.Parse(s.Substring(1), NumberStyles.AllowHexSpecifier)));
+                else
+                    return Color.FromArgb(Int32.Parse(s.Substring(1), NumberStyles.AllowHexSpecifier));
+            }
+            else
+                return Color.FromName(s);
+        }
+
+        private char[] RememberBrackets(FastColoredTextBox tb)
+        {
+            return new[] { tb.LeftBracket, tb.RightBracket, tb.LeftBracket2, tb.RightBracket2 };
+        }
+
+        private void RestoreBrackets(FastColoredTextBox tb, char[] oldBrackets)
+        {
+            tb.LeftBracket = oldBrackets[0];
+            tb.RightBracket = oldBrackets[1];
+            tb.LeftBracket2 = oldBrackets[2];
+            tb.RightBracket2 = oldBrackets[3];
+        }
+
+        private SyntaxDescriptor SyntaxDesc;
+    }
+}
