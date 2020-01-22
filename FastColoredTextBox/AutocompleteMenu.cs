638158b3d00207a1c4f4888e87fa29662d9dede9
Seba-Desktop <sebagames1@gmail.com>
2020-01-22 00:40:18 -0300

    Improvement on AutoComplete items population.

--- FastColoredTextBox/AutocompleteMenu.cs
+++ FastColoredTextBox/AutocompleteMenu.cs
@@ -419,11 +419,19 @@ namespace FastColoredTextBoxNS
                 }
 
                 visibleItems.Sort((AutocompleteItem a, AutocompleteItem b)=> {
-                    var indexA = a.Text.IndexOf(text);
-                    var indexB = b.Text.IndexOf(text);
+                    var indexA = a.Text.IndexOf(text, StringComparison.InvariantCultureIgnoreCase);
+                    var indexB = b.Text.IndexOf(text, StringComparison.InvariantCultureIgnoreCase);
+
+                    if(indexA == indexB) {
+                        if(a.Text.Length == b.Text.Length) return string.Compare(a.Text, b.Text, true);
+                        return a.Text.Length - b.Text.Length;
+                    }
+                    else {
+                        if(indexA == -1) return 1;
+                        if(indexB == -1) return -1;
+                    }
 
                     return indexA - indexB;
-
                 });
 
                 if (foundItem != null)
